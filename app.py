05986c6910608ed75ee324088942a05bc7d3a5d4
Rapsw Calapsu <rapswarts@gmail.com>
2022-07-19 14:52:33 +0200

    commit

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, redirect, url_for, request, session
-
+from formulaires import Connexion 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'Secret'
 
 @app.route("/")
 def accueil():
-    return render_template("accueil.html")
+    form = Connexion()
+    
+    return render_template("accueil.html", form = form)
+
 
 @app.route('/article/<nom>')
 def article(nom):
-    return render_template("article.html", nom=nom)
+    return render_template("article.html", titre=nom)
---
+++ formulaires.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from datetime import datetime
+from wtforms import StringField, PasswordField, EmailField
+
+class Connexion(FlaskForm):
+    login = EmailField("login")
+    password = PasswordField("password")
