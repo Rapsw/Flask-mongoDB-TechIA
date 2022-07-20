eef55e2ce7be90ef9547c1dc6bfa743a623814f7
kamel <kamelben9@gmail.com>
2022-07-20 10:29:05 +0200

    maj kamel

--- app.py
+++ app.py
@@ -2,37 +2,38 @@ from flask import Flask, render_template, redirect, url_for, request, session
 from formulaires import Connexion 
 from pymongo import MongoClient
 from formulaires import Connexion
-from forms import RegisterForm
-from config import Config
 
 app = Flask(__name__)
 url = "mongodb://localhost:27017"
 client = MongoClient(url)
 
-#app.config['SECRET_KEY'] = 'Secret'
-app.config.from_object(Config)
-
-@app.route("/")
-def accueil():
-    form = Connexion()
-    
-    return render_template("accueil.html", form = form)
+app.config['SECRET_KEY'] = 'Secret'
 
 db = client.blog
 articles = db.article  # une collection article
 
 @app.route("/", methods = ['GET','POST'])
 def accueil():
-    form = Connexion()
+    try:
+        login = session["login"]
+    except:
+        login = None
 
-    return render_template("accueil.html", form = form , articles = articles.find())
+    return render_template("accueil.html", form = form , articles = articles.find(), login=login)
 
 @app.route('/article/<nom>')
 def article(nom):
     return render_template("article.html", titre=nom)
 
-@app.route('/register')
-def register():
-    form = RegisterForm()
 
-    return render_template('register.html', form = form, title = 'Resister')
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    utilisateur = {"login" : "antoine.meresse@info", "password" : "azerty"}
+    form = Connexion()
+    if form.validate_on_submit():
+        if form.data["login"] == utilisateur["login"] and form.data["password"] == utilisateur["password"]:
+            session["login"] = utilisateur["login"]
+            return redirect(url_for("acceuil"))
+    return render_template("login.html", form=form)
+
+#maj kamel
--- formulaires.py
+++ formulaires.py
@@ -1,15 +1,9 @@
 from flask_wtf import FlaskForm
 from datetime import datetime
 from wtforms import StringField, PasswordField, EmailField, SubmitField
-from wtforms.validators import DataRequired
 
 class Connexion(FlaskForm):
     login = EmailField("login")
     password = PasswordField("password")
-    password = PasswordField("password")
-
-class RegisterForm(FlaskForm)
-    email = StringField("Email", validators=[])
-    submit = SubmitField("S'inscrire")
-
+    submit = SubmitField("Se connecter")
 
