2141c68e53175b382ab225b05c02afb850c9ad86
kamel <kamelben9@gmail.com>
2022-07-21 09:11:26 +0200

    ajout css et modif bdd

--- remplir_bdd.py
+++ remplir_bdd.py
@@ -12,14 +12,23 @@ article.drop() #nettoyer la base de données
 
 
 article.insert_one (
-    { "titre" : "TITRE 1", "résumé" : "résumé 1" , "date" :str(datetime.now()), "texte" : "Lorem " , 
+    { "titre" : "Projet Wa-Tor", 
+    "résumé" : "A l'aide de vos connaissances en Python et au logiciel Processing, vous simulerez  la vie et les interactions entre une population de proies et de prédateurs dans une mer de forme torique (forme de donuts)" ,
+     "date" :str(datetime.now()), 
+     "texte" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum " , 
     "commentaires":   [ {"user": "De Niro", "texte": "test commentaire"}]}  
    )
 
 article.insert_one (
- {"titre" : "ARTICLE 2" , "résumé" : "résumé 3" , "date" :str(datetime.now()), "texte" : "test " } )
+ {"titre" : "Logiciel d'Histoires Participatif" , 
+ "résumé" : "Le site communautaire Reddit, habitué des défis collaboratifs, a décidé de lancer une nouvelle expérimentation.Cette fois-ci, ils souhaitent regarder ce qui se passe quand on laisse de nombreux internautes écrire une histoire, tous ensemble." ,
+  "date" :str(datetime.now()), 
+  "texte" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum " } )
 
-article.insert_one ( {"titre" : "ARTICLE 3" , "résumé" : "résumé 3" , "date" :str(datetime.now()), "texte" : "test 2  " } )
+article.insert_one ( {"titre" : "Réalisation Blog avec Flask et MongoDB" ,
+    "résumé" : "Vous souhaitez réaliser un blog afin de montrer ce que vous avez vu en formation, mais aussi pour partager vos veilles quotidiennes sur le sujet de l'IA, de la data et du développement en général." , 
+    "date" :str(datetime.now()), 
+    "texte" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum " } )
 
 
 # afficher la base de données 
